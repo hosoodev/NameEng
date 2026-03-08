d32dddd7ddb41ac06157da93e55666900121587b
hosoodev <hosoodev@gmail.com>
2026-03-08 16:39:50 +0900

    메인 첫광고 모바일 광고 탑 마진 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -572,9 +572,9 @@ function NameEngConverter() {
                 <AdSlot
                   slot={AD_SLOT_INDEX_CONTENT}
                   format="auto"
-                  wrapperClassName="py-1 md:my-6 md:rounded-lg md:overflow-hidden mt-2"
+                  wrapperClassName="py-1 md:my-6 md:rounded-lg md:overflow-hidden mt-4"
                   lazyLoad={false}
-                  mobileFixed={true} // 모바일에서 320×100 고정
+                  mobileFixed // 모바일에서 320×100 고정
                 />
 
                 <div className="px-4 md:px-0 space-y-6 mt-2">
